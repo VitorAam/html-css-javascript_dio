3fb7fc179573dab629356a1648305b2e41487bd6
Vitor <v.augusto.am@gmail.com>
2022-07-24 21:27:51 -0300

    att

---
+++ javascript_inicial/desafio_operadores/README.md
@@ -0,0 +1,3 @@
+# Desafio de operadores
+
+Desafio concluído, função criada e com todas as funcionalidades utilizando operadores ternários.
---
+++ javascript_inicial/desafio_operadores/operadores.js
@@ -0,0 +1,8 @@
+function numeros(n1, n2){
+    let iguais = n1==n2? 'são iguais':'não são iguais';
+    let maiorDez = n1+n2 > 10? 'maior':'menor';
+    let maiorVinte = n1+n2 > 20? 'maior':'menor';
+    return `Os números ${n1} e ${n2} ${iguais}. Sua soma é ${n1+n2}, que é ${maiorDez} que 10 e ${maiorVinte} que 20.`
+}
+
+console.log(numeros(78,3))
